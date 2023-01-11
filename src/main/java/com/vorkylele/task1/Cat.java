83d25cb1efb0f4a6387b29db816013849c847fcf
vorkylele/Artem Vikhlyantsev <vorkylele@gmail.com>
2023-01-12 00:21:04 +0500

    added 5th item of the task 1

--- src/main/java/com/vorkylele/task1/Cat.java
+++ src/main/java/com/vorkylele/task1/Cat.java
@@ -1,7 +1,13 @@
 package com.vorkylele.task1;
 
 public class Cat extends Animal {
+    private static int countCat;
     public Cat(String name, int lengthRun, int lengthSwim) {
         super(name, lengthRun, lengthSwim);
+        countCat += 1;
+    }
+
+    public static int getCountCat() {
+        return countCat;
     }
 }
--- src/main/java/com/vorkylele/task1/Tiger.java
+++ src/main/java/com/vorkylele/task1/Tiger.java
@@ -1,7 +1,14 @@
 package com.vorkylele.task1;
 
 public class Tiger extends Animal {
+    static int countTiger;
+
     public Tiger(String name, int lengthRun, int lengthSwim) {
         super(name, lengthRun, lengthSwim);
+        countTiger += 1;
+    }
+
+    public static int getCountTiger() {
+        return countTiger;
     }
 }
